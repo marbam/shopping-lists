e21f728fac74fa8879d5db00048fa5c88af7921a
Martin Bampton <martinbampton@hotmail.com>
2020-05-13 15:41:43 +0100

    Adding in react-router-dom and initial SPA functionality.

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Listing from './Listing/Listing';
 import Create from './Create/Create';
 
 function App() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <Create/>
-                    <Listing/>
+        <BrowserRouter>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <Route path="/" exact render= {() => <Link to='/create'>Create</Link>} />
+                        <Route path="/" exact component={Listing} />
+                        <Route path="/create" exact render={() => <Link to='/'>Home</Link>} />
+                        <Route path="/create" exact component={Create} />
+                    </div>
                 </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
